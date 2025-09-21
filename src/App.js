16a890c84a5bf18fa6e330a7fd6e261dafc80e46
coderaavana-manoj <Manoj$2026!>
2025-09-21 21:42:24 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm Manoj Kumar N M and this is my first deployment React Application.</h1>
+      <h1>Hi, I'm Manoj Kumar N M and Deployed through GitHub.</h1>
     </div>
   );
 }
